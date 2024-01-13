a1562b2e206e19ce41efb50402a720e583d6b279
jeremiasmarinho <jeremias10@msn.com>
2024-01-13 03:13:30 -0300

    prisma and vercel configuration

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,3 +1,5 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export function NavBar() {
@@ -9,6 +11,21 @@ export function NavBar() {
       >
         Livraria
       </Link>
+      <div className="flex items-center gap-8">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button
+              type="button"
+              className="border rounded-md border-gray-400 px-3 py-2"
+            >
+              Fazer login
+            </button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </nav>
   );
 }
